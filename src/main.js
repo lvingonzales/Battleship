5f908b2b9d4e940fa459176c9a56888666f2aeb5
lvingonzales <lvingonzales15@gmail.com>
2025-01-06 13:25:56 -0500

    Added Cell class, Board object with appropriate methods, addShip function, Player class, player test file and empty user interface file. All of the above methods and functions passed their tests.

--- src/main.js
+++ src/main.js
@@ -1,23 +1,151 @@
-
 class Ship {
-    constructor (length){
-        this.length = length;
-        this.hp = length;
-    }
+  constructor(length) {
+    this.length = length;
+    this.hp = length;
+  }
 
-    hit(){
-        this.hp = this.hp - 1;
-        this.isSunk(this.hp);
-        return true;
-    }
+  hit() {
+    this.hp = this.hp - 1;
+    return true;
+  }
 
-    isSunk(hp) {
-        if (hp === 0){
-            return true;
-        } else {
-            return false;
-        }
+  isSunk() {
+    if (this.hp === 0) {
+      return true;
+    } else {
+      return false;
     }
+  }
 }
 
-module.exports = {Ship}
+const board = {
+  p1_board: [...Array(10)].map(() => Array(10).fill(null)),
+  p2_board: [...Array(10)].map(() => Array(10).fill(null)),
+
+  getBoard(id){
+    return id === 0 ? this.p1_board : this.p2_board;
+  },
+
+  fillBoard() {
+    for (let x = 0; x < this.p1_board.length; x++) {
+      for (let y = 0; y < this.p1_board.length; y++) {
+        this.p1_board[x][y] = new Cell(x, y);
+      }
+    }
+  },
+
+  getCell(x, y) {
+    return this.p1_board[x][y];
+  },
+
+  setCell(x, y, value) {
+    cell = this.getCell(x, y);
+    cell = cell + value;
+    return cell;
+  },
+
+  checkCell(x, y) {
+    if (x >= 0 && x <= 9 && y >= 0 && y <= 9) {
+      let cell = this.getCell(x, y);
+      if (cell.ship !== null) {
+        return false;
+      }
+      if (cell.value <= 1) {
+        return true;
+      }
+    }
+    return false;
+  },
+
+  receiveAttack(x, y) {
+    let cell = this.getCell(x, y);
+
+    if (cell.value === 0) {
+      cell.value = cell.value + 2;
+      return `Shot cell: ${x},${y} ... MISS!`;
+    } else if (cell.value === 1) {
+      cell.value = cell.value + 2;
+      cell.ship.hit();
+      if (cell.ship.isSunk() === true) {
+        return `Shot cell: ${x},${y} ... SUNK!`;
+      }
+      return `Shot cell: ${x},${y} ... HIT!`;
+    }
+  },
+};
+
+function addShip(x, y, row_col, up_down, length) {
+  if (row_col > 1 || row_col < 0 || up_down > 1 || up_down < 0) {
+    throw new Error(`Invalid Direction`);
+  }
+  if (board.p1_board[x][y].value === 0) {
+    let newShip = new Ship(length);
+
+    // 50/50 chance to determine the direction boats are placed in,
+    // let row_colChoice = (Math.random() >= 0.5)? 1 : 0; // 0: row, 1 col
+    // let directionChoice = (Math.random() >= 0.5)? 1 : 0; // 0: up/right, 1: down/left
+    if (!checkShipCells(x, y, row_col, up_down, length)) {
+      throw new Error(`Invalid Placement`);
+    }
+
+    for (let i = 0; i < length; i++){
+        if (row_col === 0) {
+            if (up_down === 0) {
+                board.p1_board[x][y].ship = newShip;
+                board.p1_board[x][y].value = 1;
+                y = y + 1;
+            } else {
+                board.p1_board[x][y].ship = newShip;
+                board.p1_board[x][y].value = 1;
+                y = y - 1;
+            }
+        } else {
+            if (up_down === 0) {
+                board.p1_board[x][y].ship = newShip;
+                board.p1_board[x][y].value = 1;
+                x = x - 1;
+            } else {
+                board.p1_board[x][y].ship = newShip;
+                board.p1_board[x][y].value = 1;
+                x = x + 1;
+            }
+        }
+    }
+  }
+
+  return board.p1_board[x][y].ship;
+}
+
+function checkShipCells(x, y, row_col, up_down, length) {
+  for (let i = 0; i < length; i++) {
+    if (!board.checkCell(x, y)) {
+      return false;
+    }
+    if (row_col === 1) {
+      if (up_down === 0) {
+        x = x - 1;
+      } else {
+        x = x + 1;
+      }
+    } else {
+      if (up_down === 0) {
+        y = y + 1;
+      } else {
+        y = y - 1;
+      }
+    }
+  }
+
+  return true;
+}
+
+class Cell {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.value = 0;
+    this.ship = null;
+  }
+}
+
+module.exports = {board};
--- src/main.test.js
+++ src/main.test.js
@@ -1,6 +1,94 @@
-const {Ship} = require('./main');
+// const { Ship, board, addShip } = require("./main");
 
-test ('Ship should return true when sunk', () => {
-    let ship = new Ship(3);
-    expect(ship.isSunk(0)).toBe(true);
-});
+// test ('Ship should return true when sunk', () => {
+//     let ship = new Ship(3);
+//     ship.hit();
+//     ship.hit();
+//     ship.hit();
+//     expect(ship.isSunk()).toBe(true);
+// });
+
+// test ('Ship.isSunk() should return false when not sunk', () => {
+//     let ship = new Ship(3);
+//     ship.hit();
+//     ship.hit();
+//     expect(ship.isSunk()).toBe(false);
+// });
+
+// test ('board.setCell should return 1,1', () => {
+//     expect(board.setCell(1,1,1)).toBe(1,1);
+// })
+
+// test ('board.setCell(1,1,3) should return 3,3', () => {
+//     expect(board.setCell(1,1,3)).toBe(3,3);
+// })
+
+// Test receive attack function
+
+// const fillBoard = jest.fn(() => {
+//   board.fillBoard();
+// });
+
+// test('Out of bound cell coordinates should return false, eg(12,13)', () => {
+//     fillBoard();
+//     expect(board.checkCell(12,13)).toBe(false);
+// })
+
+// test('Valid cell coordinates should return true, eg(3,3)', () => {
+//     fillBoard();
+//     expect(board.checkCell(3,3)).toBe(true);
+// })
+
+// test('Trying to shoot a cell with a value greater than 1 should return false', () => {
+//     fillBoard();
+//     board.receiveAttack(1,1);
+//     expect(board.checkCell(1,1)).toBe(false);
+// })
+
+// test('Receiving an attack on an empty square should return the string "Shot cell: 1,1 ... MISS!"', () => {
+//     fillBoard();
+//     expect(board.receiveAttack(1,1)).toBe("Shot cell: 1,1 ... MISS!");
+// })
+
+// test('Receiving an attack on an occupied square should return the string "Shot cell: 3,3 ... SUNK!" if the boat sank', () => {
+//     fillBoard();
+//     expect(board.receiveAttack(3,3)).toBe("Shot cell: 3,3 ... SUNK!");
+// })
+
+// test('Receiving an attack on an occupied square should return the string "Shot cell: 3,3 ... HIT!" if the boat was hit but not sunk', () => {
+//     fillBoard();
+//     expect(board.receiveAttack(3,3)).toBe("Shot cell: 3,3 ... SUNK!");
+// })
+
+// Testing addShip() function
+// test("Adding a ship that passes out of bounds throws an error. ie: addShip(0, 0, 1, 0, 2)", () => {
+//   fillBoard();
+//   expect(() => {
+//     addShip(0, 0, 1, 0, 2);
+//   }).toThrow();
+// });
+
+// not checking for an invalid initial cell as I plan to reuse checkCell() tested above
+
+// test("Adding a ship to a valid cell that does not pass out of bounds should return a non null cell.ship property", () => {
+//   fillBoard();
+//   expect(addShip(1, 1, 1, 1, 1)).not.toBeNull();
+// });
+
+// test("trying to add a ship to a spot that contains another ship returns false", () => {
+//     fillBoard();
+//     addShip(1,1,1,1,1);
+//     expect(board.checkCell(1,1)).toBe(false);
+// })
+
+// test("trying to add a ship to a spot that would cause it to overlap another ship throws an error", () => {
+//     fillBoard();
+//     addShip(1,1,1,1,1);
+//     expect(() => {
+//         addShip(1,3,0,1,3)
+//     }).toThrow();
+// })
+
+// Testing Player class methods
+
+
